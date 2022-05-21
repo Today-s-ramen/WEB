6a1600b16af45b95204180361f7d4d8e89ec728c
JY <pianowell96@gmail.com>
2022-05-21 15:12:51 +0900

    [feat]: Banner, DatailPages 컴포넌트 추가

---
+++ src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import DetailIntro from "../components/DetailIntro";
+import IntroLayout from "../components/IntroLayout";
+import MainBanner from "../components/MainBanner";
+
+const MainPage = () => {
+  return (
+    <>
+      <MainBanner />
+      <IntroLayout />
+      <DetailIntro />
+    </>
+  );
+};
+
+export default MainPage;
