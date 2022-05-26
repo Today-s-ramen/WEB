40e1fd7d534b9b6950dd3b49f6b98bc6e9b3273e
HyeJun <solar3070@naver.com>
2022-05-27 00:28:28 +0900

    [feat]: 이미지 슬라이드 기능 구현

--- src/components/review/ReviewImage.jsx
+++ src/components/review/ReviewImage.jsx
@@ -3,16 +3,47 @@ import styled from 'styled-components';
 import Left from '../../assets/Left.svg';
 import Right from '../../assets/Right.svg';
 
+import { useState, useRef, useEffect } from 'react';
+
 const ReviewImage = ({ imageList }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideRef = useRef(null);
+  const TotalSlide = imageList.length - 1;
+
+  const prevSlide = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(TotalSlide);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
+  const nextSlide = () => {
+    if (currentSlide >= TotalSlide) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+
+  useEffect(() => {
+    slideRef.current.style.transition = 'all 0.5s ease-in-out';
+    slideRef.current.style.transform = `translateX(-${currentSlide}00%)`;
+  }, [currentSlide]);
+
   return (
     <ReviewImageWrapper>
-      <LeftButton>
+      <LeftButton onClick={prevSlide}>
         <img src={Left} alt="왼쪽 화살표" />
       </LeftButton>
-      {imageList.slice(0, 1).map((item) => {
-        return <Image key={item.id} src={item.img} alt="리뷰 사진" />;
-      })}
-      <RightButton>
+      <SlideWrapper>
+        <SlideBox ref={slideRef}>
+          {imageList.map((item) => {
+            return <Image key={item.id} src={item.img} alt="리뷰 사진" />;
+          })}
+        </SlideBox>
+      </SlideWrapper>
+      <RightButton onClick={nextSlide}>
         <img src={Right} alt="오른쪽 화살표" />
       </RightButton>
     </ReviewImageWrapper>
@@ -27,6 +58,16 @@ const ReviewImageWrapper = styled.div`
   position: relative;
 `;
 
+const SlideWrapper = styled.div`
+  overflow-x: hidden;
+  width: 800px;
+  height: 800px;
+`;
+
+const SlideBox = styled.div`
+  display: flex;
+`;
+
 const Image = styled.img`
   width: 800px;
   height: 800px;
@@ -34,6 +75,7 @@ const Image = styled.img`
 
 const SlideButton = styled.div`
   position: absolute;
+  z-index: 1;
   width: 100px;
   height: 100px;
   border-radius: 50px;
