36a15e4e3d80d5cb33f4d73636a8e6689f2381c7
HyeJun <solar3070@naver.com>
2022-05-26 22:51:24 +0900

    [feat]: 이미지 슬라이드 버튼 추가

--- src/components/review/ReviewImage.jsx
+++ src/components/review/ReviewImage.jsx
@@ -1,20 +1,57 @@
 import styled from 'styled-components';
 
+import Left from '../../assets/Left.svg';
+import Right from '../../assets/Right.svg';
+
 const ReviewImage = ({ imageList }) => {
   return (
     <ReviewImageWrapper>
-      {imageList.map((item) => {
+      <LeftButton>
+        <img src={Left} alt="왼쪽 화살표" />
+      </LeftButton>
+      {imageList.slice(0, 1).map((item) => {
         return <Image key={item.id} src={item.img} alt="리뷰 사진" />;
       })}
+      <RightButton>
+        <img src={Right} alt="오른쪽 화살표" />
+      </RightButton>
     </ReviewImageWrapper>
   );
 };
 
 export default ReviewImage;
 
-const ReviewImageWrapper = styled.div``;
+const ReviewImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
 
 const Image = styled.img`
   width: 800px;
   height: 800px;
+`;
+
+const SlideButton = styled.div`
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background-color: ${({ theme }) => theme.color.red};
+  cursor: pointer;
+
+  img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const LeftButton = styled(SlideButton)`
+  left: -48px;
+`;
+
+const RightButton = styled(SlideButton)`
+  right: -48px;
 `;
