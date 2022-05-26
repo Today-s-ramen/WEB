f01a493a259d6a0950ab81bee959d0e0b5f34f27
HyeJun <solar3070@naver.com>
2022-05-26 13:12:13 +0900

    [feat] 리뷰 컴포넌트들 추가

--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -1,10 +1,28 @@
+import styled from 'styled-components';
+import ReviewInfo from '../components/review/ReviewInfo';
+import ImageSlide from '../components/review/ImageSlide';
+import ReviewText from '../components/review/ReviewText';
+
 function ReviewPage() {
   return (
     <>
-      <h1>asdf</h1>
-      <h3>adsfadsf</h3>
+      <h2>asd</h2>
+      <StyledRoot>
+        <h1>asdf</h1>
+        <ReviewInfo />
+        <ImageSlide />
+        <ReviewText />
+      </StyledRoot>
     </>
   );
 }
 
 export default ReviewPage;
+
+const StyledRoot = styled.div`
+  font-family: 'GmarketSansMedium';
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
