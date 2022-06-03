cc4b12669fd582dc013aaa4935ae384a6ea0420a
ingong <insong1216@gmail.com>
2022-06-03 13:56:31 +0900

    [feat]: subscribe 컴포넌트 내에서 API 호출

--- src/components/Subscribe/index.jsx
+++ src/components/Subscribe/index.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import SubscribeTitle from './SubscribeTitle';
 import ShipmentPeriod from './ShipmentPeriod';
@@ -7,38 +7,42 @@ import ShipmentQuantity from './ShipmentQuantity';
 import PeriodNotify from './PeriodNotify';
 import SubscribePageMove from './SubscribePageMove';
 
+import { getDeliveryOptions } from 'api/subscribe';
+
 const SubscribeWrapper = styled.div`
   width: 100vw;
   padding: 0 120px;
   margin-top: 30px;
 `;
 
-const initialPeriodList = [
-  { key: 0, period: '한 달에 1회', selected: false },
-  { key: 1, period: '두 달에 1회', selected: false },
-  { key: 2, period: '세 달에 1회', selected: false },
-  { key: 3, period: '여섯 달에 1회', selected: false },
-];
-
-const initialQuantityList = [
-  { key: 0, quantity: '5개(Mini)', price: 7900, selected: false },
-  { key: 1, quantity: '10개(Single) + 랜덤 라면토핑', price: 13900, selected: false },
-  { key: 2, quantity: '20개(Double) + 랜덤 라면토핑', price: 24900, selected: false },
-];
-
 const Subscribe = () => {
-  const [periodList, setSelectedPeriod] = useState(initialPeriodList);
-  const [quantityList, setSelectedQuantity] = useState(initialQuantityList);
+  const [periodList, setSelectedPeriod] = useState([]);
+  const [quantityList, setSelectedQuantity] = useState([]);
+
+  useEffect(() => {
+    getDeliveryOptions()
+      .then(({ deliveryPeriodOptions, deliveryQuantityOptions }) => {
+        return {
+          deliveryPeriodOptions: deliveryPeriodOptions.map(({ option, _id }) => {
+            return { key: _id, period: option, selected: false };
+          }),
+          deliveryQuantityOptions: deliveryQuantityOptions.map(({ option, _id }) => {
+            return { key: _id, quantity: option, price: 10000, selected: false };
+          }),
+        };
+      })
+      .then(({ deliveryPeriodOptions, deliveryQuantityOptions }) => {
+        setSelectedPeriod(deliveryPeriodOptions);
+        setSelectedQuantity(deliveryQuantityOptions);
+      })
+      .catch((e) => console.log(e));
+  }, []);
 
   return (
     <SubscribeWrapper>
       <SubscribeTitle />
-      <ShipmentPeriod periodList={periodList} initialList={initialPeriodList} setSelectedPeriod={setSelectedPeriod} />
-      <ShipmentQuantity
-        quantityList={quantityList}
-        initialList={initialQuantityList}
-        setSelectedQuantity={setSelectedQuantity}
-      />
+      <ShipmentPeriod periodList={periodList} setSelectedPeriod={setSelectedPeriod} />
+      <ShipmentQuantity quantityList={quantityList} setSelectedQuantity={setSelectedQuantity} />
       <SubscribePageMove />
       <PeriodNotify />
     </SubscribeWrapper>
