876e9fe0a07296078d8c8cf07f03d407a25367aa
ingong <insong1216@gmail.com>
2022-06-03 16:38:59 +0900

    [fix]: API BASE URL 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.NODE_ENV === 'development' ? '' : process.env.REACT_APP_API_DOMAIN;
+const API_BASE_URL = process.env.NODE_ENV === 'development' ? '/' : process.env.REACT_APP_API_DOMAIN;
 
 export default API_BASE_URL;
--- src/api/subscribe.js
+++ src/api/subscribe.js
@@ -1,7 +1,7 @@
 import API_BASE_URL from 'api';
 
 const getDeliveryOptions = async () => {
-  const response = await fetch(`${API_BASE_URL}/subscribe`);
+  const response = await fetch(`${API_BASE_URL}` + 'subscribe');
   const {
     success,
     data: { deliveryPeriodOptions, deliveryQuantityOptions },
@@ -23,7 +23,7 @@ const postSubscribeOptions = async ({ selectedPeriod, selectedQuantity }) => {
       option: selectedQuantity,
     },
   };
-  const { ok } = await fetch(`${API_BASE_URL}/subscribe`, {
+  const { ok } = await fetch(`${API_BASE_URL}` + 'subscribe', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
