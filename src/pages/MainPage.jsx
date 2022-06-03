2e050f1a3757fa428724071cd8c9e64e02d457e1
ingong <insong1216@gmail.com>
2022-06-03 15:17:14 +0900

    [feat]: 메인 페이지 헤더 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,16 +4,19 @@ import SubscribeBanner from 'components/main/SubscribeBanner';
 import DetailIntro from 'components/main/DetailIntro';
 import IntroLayout from 'components/main/IntroLayout';
 import MainBanner from 'components/main/MainBanner';
+import { Header, Footer } from 'components';
 
 const MainPage = () => {
   return (
     <>
+      <Header />
       <MainBanner />
       <IntroLayout />
       <DetailIntro />
       <ReviewSlide />
       <CouponBanner />
       <SubscribeBanner />
+      <Footer />
     </>
   );
 };
